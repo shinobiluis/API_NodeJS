bac602ade9735a386719edc343529efbcc2cb48f
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-07-11 14:07:04 -0500

    Conectamos y agremagos un estudiante a la base

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("hola");
+// importamos mongoose
+const mongoose = require('mongoose');
+
+// conectamos a la base de datos
+mongoose.connect('mongodb://192.168.0.5:27017/school', {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+}, () => {
+    console.log('si conecto');
+});
+
+const Student = mongoose.model('students', {
+    name:{
+        type: String,
+        required: true
+    },
+    course: {
+        type: String,
+        required: true,
+        enum:[
+            'Programacion',
+            'Inglés',
+            'Cocina'
+        ]
+    },
+    age: {
+        type: Number,
+        min: 1,
+    }
+})
+
+Student.create({
+    name:'Luis',
+    age: 30,
+    course: 'Inglés'
+})
