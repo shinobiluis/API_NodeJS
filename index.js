b7ba79efa89b6bfd43c40d85f3448f7d0e463082
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-07-11 14:44:54 -0500

    Creamos rutas para agregar, eliminar y consultar

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
+// imprtamos express
+const express = require('express');
 // importamos mongoose
 const mongoose = require('mongoose');
+const { request } = require('express');
 
+const app = express();
+app.use(express.json());
+
+app.get('/students', async(request, response) => {
+    const allStudents = await Student.find();
+    response.json({
+        success:true,
+        data: {
+             students: allStudents
+        }
+    })
+});
+app.post('/students', async(request, response) =>{
+    const studentInfo = request.body;
+    const newStudent = await Student.create(studentInfo);
+    response.json({
+        success: true,
+        data: {
+            student: newStudent
+        }
+    });
+});
+app.delete('/students/:id', async(request, response) => {
+    const id = request.params.id
+    const studentDeleted = await Student.findByIdAndDelete(id);
+    response.json({
+        success:true,
+        data:{
+            student: studentDeleted
+        }
+    })
+});
 // conectamos a la base de datos
 mongoose.connect('mongodb://192.168.0.5:27017/school', {
     useNewUrlParser: true, 
     useUnifiedTopology: true
 }, () => {
-    console.log('si conecto');
+    console.log('Se conecto a base de datos');
+    app.listen(8080, () => {
+        console.log('El servidor esta listo')
+    })
 });
 
 const Student = mongoose.model('students', {
@@ -18,7 +56,7 @@ const Student = mongoose.model('students', {
         type: String,
         required: true,
         enum:[
-            'Programacion',
+            'Programación',
             'Inglés',
             'Cocina'
         ]
@@ -29,8 +67,8 @@ const Student = mongoose.model('students', {
     }
 })
 
-Student.create({
-    name:'Luis',
-    age: 30,
-    course: 'Inglés'
-})
+// Student.create({
+//     name:'Luis',
+//     age: 30,
+//     course: 'Inglés'
+// })
